df635b1dc7628ec9aebe71eb913ce20d3d2121fb
Abdulrahman Sayed <abdu.ewian@gmail.com>
2025-10-29 12:38:33 +0300

    add

--- MobileLinesManager/Converters/StatusToVisibilityConverter.cs
+++ MobileLinesManager/Converters/StatusToVisibilityConverter.cs
@@ -9,30 +9,6 @@ namespace MobileLinesManager.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is string status && parameter is string expectedStatus)
-            {
-                return status == expectedStatus ? Visibility.Visible : Visibility.Collapsed;
-            }
-            return Visibility.Collapsed;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-}
-using System;
-using System.Globalization;
-using System.Windows;
-using System.Windows.Data;
-
-namespace MobileLinesManager.Converters
-{
-    public class StatusToVisibilityConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
             if (value == null || parameter == null)
                 return Visibility.Collapsed;
 
